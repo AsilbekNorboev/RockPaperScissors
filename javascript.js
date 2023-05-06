c395e8db733dbc6239f4e448ce7c1694b2dd222a
Asilbek Norboev <asilbeknorboev@gmail.com>
2023-05-06 18:21:07 -0500

    finished ui

--- javascript.js
+++ javascript.js
@@ -4,15 +4,56 @@ function getComputerChoice(){
     return randomChoice;
 };
 
+const selection = document.querySelectorAll('.selections button');
+const resultstext = document.querySelector('.results');
+const playagain = document.querySelector('.play-again button')
+
+playagain.addEventListener('click', () => location.reload());
+selection.forEach(button =>{
+    button.addEventListener('click',getPlayerChoice)
+});
+
+function getPlayerChoice(e){
+    let playerSelection = (e.target.id);
+    playerChoice = e.target.textContent;
+    console.log(playerChoice);
+    const results = playround(playerChoice, getComputerChoice());
+    console.log(results[1])
+    resultstext.textContent = results[1];
+}
+
+const playercount = document.querySelector('.player');
+const draws = document.querySelector('.draws');
+const computercount = document.querySelector('.computer');
+
+let userwins = computerwins = ties = 0
+
+function isgameover(){
+    return userwins===5 || computerwins===5;
+}
+let winner;
+
+function checkWinner(){
+    if (userwins===5){
+        return 'user';
+    }
+    else{
+        return 'computer';
+    }
+}
+
 function playround(playerchoice, computerchoice){
     let result;
     playerchoice=playerchoice.toLowerCase();
     if (playerchoice===computerchoice){
         result = "tie";
+        ties+=1
+        draws.textContent = `DRAWS: ${ties}`;
         return [0,"It was a tie!"]
     }
     else if (playerchoice=="rock" && computerchoice=="scissors"){
         result = "win";
+        
     }
     else if (playerchoice=="paper" && computerchoice=="rock"){
         result = "win";
@@ -25,44 +66,35 @@ function playround(playerchoice, computerchoice){
     }
 
     if (result=="win"){
+        userwins+=1
+        playercount.textContent = `PLAYER WINS: ${userwins}`
+    }
+    else {
+            computerwins+=1
+            computercount.textContent = `COMPUTER WINS: ${computerwins}`};
+
+
+    if (isgameover()){
+
+        console.log("GAME OVER");
+        winner = checkWinner();
+        console.log(winner);
+        selection.forEach(button => {
+            button.removeEventListener('click', getPlayerChoice);
+        })
+        if (winner=='user'){
+            return [3, 'YOU WON THE GAME!'];
+        }
+        else{
+            return [3, "OOF! YOU LOST!"];
+        }
+    }
+
+    if (result=="win"){
         return [1,`You Won! ${playerchoice} beats ${computerchoice}`];
     }
     else {
         return [-1,`You Lose! ${computerchoice} beats ${playerchoice}`];
     }
 
-}
-
-function game(){
-    let userwins = computerwins = ties =0;
-    for (let i =0; i<5; i++){
-        let playerSelection = prompt("Choose what to play this round: (Rock, Paper, Scissors)")
-        const computerSelection = getComputerChoice();
-        result = playround(playerSelection, computerSelection);
-        winner = result[0]
-        console.log(result[1])
-        if (winner==1){
-            userwins+=1;
-        }
-        else if (winner==-1){
-            computerwins+=1;
-        }
-        else{
-            ties+=1
-        }
-    }
-    let endmessage;
-    if (userwins>computerwins){
-        endmessage=`User wins overall! --> ${userwins}`;
-    }
-    else if (computerwins>userwins){
-        endmessage =`Computer wins overall! --> ${computerwins}`;
-    }
-    else {
-        endmessage = `NO ONE WON! User: ${userwins} Computer: ${computerwins} Ties: ${ties}`;
-    }
-    console.log(endmessage);
-    alert(endmessage);
-    
-}
-game()
+};
